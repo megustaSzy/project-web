373e355bfa3b601a9f22e064cb87911b2275cd12
Raditya Ahmad <raditiaahmad50@gmail.com>
2025-10-28 09:36:16 +0700

    handler request GET

--- backend/src/app/api/users/route.ts
+++ backend/src/app/api/users/route.ts
@@ -1,4 +1,12 @@
 import { PrismaClient } from "@/generated/prisma";
 
 
-const prisma = new PrismaClient
+const prisma = new PrismaClient();
+
+export const GET = async() => {
+    const user = await prisma.tb_user.findMany({
+        orderBy: {
+            id: 'asc',
+        }
+    })
+}
