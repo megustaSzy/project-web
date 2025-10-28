1c6faea6b2a94d4cf0cb0241aa059852391ff60a
Fadly Mustofainal Ahyar <fadlymustofa0@gmail.com>
2025-10-29 00:00:06 +0700

    update layout or content on page.tsx

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar/Navbar";
-import {Hero} from "./components/Hero/Hero";
+import Hero from "./components/Hero/Hero";
 
 export default function Home() {
   return (
    <main>
+        
         <Navbar />
+        <Hero />
       </main>
 
   );
