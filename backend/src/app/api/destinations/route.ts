05b9fa478106fa5207d4b5c2a123dce4242684a3
Raditya Ahmad <raditiaahmad50@gmail.com>
2025-11-03 13:47:50 +0700

    update GET POST destination

--- backend/src/app/api/destinations/route.ts
+++ backend/src/app/api/destinations/route.ts
@@ -1,18 +1,77 @@
-import { createDestination, getAllDestinations } from "@/services/destinationService";
+import { PrismaClient } from "@/generated/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
-// response GET
-export const GET = async () => {
-    const destinations = await getAllDestinations();
 
-    return NextResponse.json({
-        destinations
-    })
+const prisma = new PrismaClient();
+
+
+// GET destinations
+export const GET = async() => {
+    try {
+        const destination = await prisma.tb_destination.findMany({
+            orderBy: {
+                id: 'asc'
+            }
+        })
+
+        return NextResponse.json({
+            destination
+        })
+    } catch (error) {
+        console.log(error)
+
+        return NextResponse.json({
+            message: "gagal mengambil data tujuan",
+            success: false
+        })
+    }
 }
 
-export const POST = async (req: NextRequest) => {
-    const data = await req.json();
-    const result = await createDestination(data);
 
-    return NextResponse.json(result)
-};
+// POST destination
+
+export const POST = async (request: NextRequest) => {
+    try {
+        
+        const data = await request.json();
+
+        const check = await prisma.tb_destination.findFirst({
+            where: {
+                name: data.name
+            },
+            select: {
+                name: true
+            }
+        })
+
+        if(check) {
+            return {
+                message: "nama destinasi gagal disimpan",
+                success: false
+            };
+        }
+
+        await prisma.tb_destination.create({
+            data: {
+                name: data.name,
+                description: data.description,
+                imageUrl: data.imageUrl,
+                price: data.price,
+                location: data.location
+            }
+        })
+
+        return NextResponse.json({
+            message: "data destinasi berhasil ditambahkan",
+            success: true
+        })
+
+    } catch (error) {
+        console.log(error)
+
+        return NextResponse.json({
+            message: "gagal membuat destinasi",
+            success: false
+        })
+    }
+}
