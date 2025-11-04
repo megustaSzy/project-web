bb24271e59e23b5edd0de42e5496e2781f80f17f
Fadly Mustofainal Ahyar <fadlymustofa0@gmail.com>
2025-11-04 10:52:20 +0700

    merubah tampilan NavButton

--- web/src/app/components/Navbar/NavButton.tsx
+++ web/src/app/components/Navbar/NavButton.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/Avatar";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 
 export default function NavButton() {
